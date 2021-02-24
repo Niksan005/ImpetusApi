95debea6e1862648073aa9907f12efd4a0a626b6
Nikola Pavlovski <nikola.j.pavlovski@outlook.com>
2021-02-24 15:02:52 +0200

    Client Side

---
+++ HttpPostReq/HttpPostReq/InputClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpPostReq
+{
+    public class InputClass
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ HttpPostReq/HttpPostReq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Zendid.Models;
+
+namespace HttpPostReq
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        private async void Login()
+        {
+            InputClass log = new InputClass { 
+                Username = "Nick",
+                Password = "n123123"
+            };
+
+            var res = await ApiClient.RequestServerPost<InputClass, string>
+                ("https://zendid.in.kutiika.net/account/login", log);
+
+            Console.WriteLine(res);
+        }
+    }
+}
